412d7245aabdb555b08907fc29e5b6e011121ada
eltonliz <eltonliz@users.noreply.github.com>
2026-09-02 11:06:22 +0800

    deploy(v3): 更新 v2·0902 全量（排课可见性/直播间穿插配置/看板精简/需求说明同步）

--- v3/assets/channel-DqAsLzvp.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./ProtoViewerPage-DJFFYmS2.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- v3/assets/static-mode-CnjaQ-eM.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index-BP3UDObq.js";const o="handoff-static-mode";function e(){return t(o,!1)}export{o as S,e as u};
