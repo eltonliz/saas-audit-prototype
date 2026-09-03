3f40cd16876d73d30a1f80e43c4dbd3df404ec36
eltonliz <eltonliz@users.noreply.github.com>
2026-09-03 18:16:56 +0800

    update v4: 无审核流-草稿发布+修创建阻断

---
+++ v4/assets/static-mode-BnmpFDBV.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index-CkfVyY29.js";const o="handoff-static-mode";function e(){return t(o,!1)}export{o as S,e as u};
--- v4/assets/static-mode-CtlJZl85.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index-DgtY1T-c.js";const o="handoff-static-mode";function e(){return t(o,!1)}export{o as S,e as u};
