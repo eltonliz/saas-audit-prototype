05781aee648fd123a5f40c08838c8a0a268e7020
eltonliz <eltonliz@users.noreply.github.com>
2026-09-07 17:59:17 +0800

    update v4: 课程结合菜单去积分任务入口

--- v4/assets/static-mode-CGGZdSsw.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index-1elLVnbA.js";const o="handoff-static-mode";function e(){return t(o,!1)}export{o as S,e as u};
---
+++ v4/assets/static-mode-GwwBJ4ds.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index-CchOKtsD.js";const o="handoff-static-mode";function e(){return t(o,!1)}export{o as S,e as u};
